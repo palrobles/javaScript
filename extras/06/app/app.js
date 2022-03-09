2397fc97443b4eeed7d28d4ecfb8bfadf79958d9
pal.robles <paloma.robles@analyticalways.com>
2022-03-09 16:18:54 +0100

    regex

--- extras/06/app/app.js
+++ extras/06/app/app.js
@@ -1,18 +1,19 @@
+const regExp =  /^\d{1,3}(\.\d{1,3})?$/;
 const sum = (first, second) => {
 
-  if( !isNaN(first) && !isNaN(second)) {
+  if( first.toString().match(regExp) && first.toString().match(regExp)) {
     return first + second;
   } else {
     return console.log('Los valores no son números');
   };
 
-  // return (!isNaN(first) && !isNaN(second)) ? first + second : console.log('Los valores no son correctos');
+  // return ( first.toString().match(regExp) && first.toString().match(regExp)) ? first + second : console.log('Los valores no son correctos');
 }
 
 console.log(sum (25, 5));
+console.log(sum (2.5, 5.3));
 console.log(sum (0, 0));
 console.log(sum (10, 0));
 console.log(sum ('a', 'b'));
 console.log(sum (' ', ' '));
-
 
