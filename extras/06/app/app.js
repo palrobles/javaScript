8d2d80ed787250f12784f45c77e516e0d7a7d3b2
pal.robles <paloma.robles@analyticalways.com>
2022-03-09 12:18:24 +0100

    ternary instruction

--- extras/06/app/app.js
+++ extras/06/app/app.js
@@ -1,10 +1,14 @@
 const sum = (first, second) => {
   let result = 0;
-  if(Number(first) && Number(second)) {
-    console.log(result = first + second);
-  } else {
-    return console.log('Los valores no son correctos')
-  }
-};
 
-sum ('', '');
+  if( (Number(first) && Number(second))  || (first === 0 || second === 0)) {
+    return result = first + second;
+  } else {
+    return console.log('Los valores no son números');
+  };
+
+  // return (Number(first) && Number(second))  || (first === 0 || second === 0) ? result = first + second : console.log('Los valores no son correctos');
+}
+
+console.log(sum (25, 5));
+
