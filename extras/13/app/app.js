aa15e03620975e39916ce81cfe9ad35e29e5a3ff
pal.robles <paloma.robles@analyticalways.com>
2022-03-11 09:17:25 +0100

    vamos con el if en el ejercicio del infierno2

--- extras/13/app/app.js
+++ extras/13/app/app.js
@@ -1,15 +1,20 @@
 let arr = [[2,1,3,4,6], [5,10,15,20,25],[25, 45, 67, 80,90], [], [2, 2, 4, 5, 2]]
 
 const removeSmallest = (arr) => {
-  newArray = [];
+  let newArray = [];
   
   for (i = 0; i < arr.length; i++) {
    let min = Math.min(...arr[i]);
-   let newArray = arr[i].filter(arr => arr !== min);
-   console.log(newArray);
+  //  let position = arr[i].indexOf(min);
+  //  console.log(position);
+  newArray.push(arr[i].filter(arr => arr !== min));
+  //  console.log(newArray);
+  console.log(...arr[i]);
   }
+ 
+  console.log(newArray);
   return newArray;
 };
 
 removeSmallest(arr);
-console.log(arr);
+// console.log(arr);
