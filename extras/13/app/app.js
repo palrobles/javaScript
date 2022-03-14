418a28498fea867d6064deb40f1ee12eec591b29
pal.robles <paloma.robles@analyticalways.com>
2022-03-14 08:30:26 +0100

    bucle anidado

--- extras/13/app/app.js
+++ extras/13/app/app.js
@@ -1,20 +1,35 @@
-let arr = [[2,1,3,4,6], [5,10,15,20,25],[25, 45, 67, 80,90], [], [2, 2, 4, 5, 2]]
+let arr = [[1,2,3,4,5], [5,3,2,1,4],[2,2,1,2,1], [], [2, 2, 4, 5, 2]];
 
+// **************************************SOLUCIÓN CON UN SOLO BUCLE
+
+// const removeSmallest = (arr) => {
+//   let newArray = [];
+  
+//   for (i = 0; i < arr.length; i++) {
+//     let min = Math.min(...arr[i]);
+//     let position = arr[i].indexOf(min);
+//     arr[i].splice(position, 1);
+//     newArray.push(arr[i]);
+//   }
+//   return newArray;
+// };
+
+// **************************************SOLUCIÓN CON UN SOLO BUCLE ANIDADO
 const removeSmallest = (arr) => {
   let newArray = [];
   
   for (i = 0; i < arr.length; i++) {
-   let min = Math.min(...arr[i]);
-  //  let position = arr[i].indexOf(min);
-  //  console.log(position);
-  newArray.push(arr[i].filter(arr => arr !== min));
-  //  console.log(newArray);
-  console.log(...arr[i]);
+    let min = arr[i][0];
+    for (j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] < min) {
+        min = arr[i][j];
+      }
+    }
+    let position = arr[i].indexOf(min);
+    arr[i].splice(position, 1);
+    newArray.push(arr[i]);
   }
- 
-  console.log(newArray);
   return newArray;
 };
-
 removeSmallest(arr);
 // console.log(arr);
---
+++ extras/14/app/app.js
@@ -0,0 +1,9 @@
+const calculateAgeDays = (year, mounth, day) => {
+  const yearNow = getFullYear();
+  const dayYear = 365;
+
+  let calculateYearAge = yearNow - year;
+  let calculateMounthAge = (calculateYearAge * mountYear) - mounth;
+
+
+};
